15bf0e627298266bbbe5decad975894c661da817
Jonathan Aguiar <jonathan.aguiar@uala.com.ar>
2022-05-30 14:06:18 -0300

    Initial commit

---
+++ aws-utils-lib/__init__.py
@@ -0,0 +1,11 @@
+import imp
+from .Functions import globalSession
+from .Functions import queryWithIndex
+from .Functions import queryWithKey
+from .Functions import isession
+from .Functions import db
+from .Functions import update
+from .Functions import updateWithSortKey
+from .Functions import deleteItem
+from .Functions import deleteItemWithSortKey
+from .Functions import putItem
