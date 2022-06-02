16bccdb6bbd4dbf7888c0f18cba899d56cdbc2aa
Jonathan Aguiar <jonathan.aguiar@uala.com.ar>
2022-06-02 19:57:09 -0300

    Normalize commit

--- prod/aws_utils_lib-base/setup.py
+++ prod/aws_utils_lib-base/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.0' 
+VERSION = '1.0.9' 
 PACKAGE_NAME = 'aws_utils_lib' 
 AUTHOR = 'Jonathan E. Aguiar' 
 AUTHOR_EMAIL = 'jonathtanm@gmail.com' 
