f228f572e8e6ce61e8d1bbd6439d48486af03112
fersor22 <fernando.sorribes@uala.com.ar>
2025-01-22 16:12:23 -0300

    update version

--- prod/aws_utils_lib-base/setup.py
+++ prod/aws_utils_lib-base/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.5.00' 
+VERSION = '1.5.1' 
 PACKAGE_NAME = 'aws_utils_lib' 
 AUTHOR = 'Jonathan E. Aguiar' 
 AUTHOR_EMAIL = 'jonathtanm@gmail.com' 
